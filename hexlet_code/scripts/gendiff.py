1f5fad11e8b409879946374aaaebb417f370d997
Anastasia Kanina <anastaskanina1988@gmail.com>
2026-06-14 15:23:52 +0300

    add format option

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -9,6 +9,13 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
+    parser.add_argument(
+        '-f',
+        '--format',
+        metavar='FORMAT',
+        help='set format of output',
+    )
+
     parser.parse_args()
 
 
