3e1744d17c1dbcee9e8fc1ca0dc027eca6ee7198
Anastasia Kanina <anastaskanina1988@gmail.com>
2026-06-14 16:07:37 +0300

    add tests for flat json diff

---
+++ tests/test_diff.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from hexlet_code import generate_diff
+
+
+def test_generate_diff():
+    base_path = Path('tests/test_data')
+
+    expected = (base_path / 'expected.txt').read_text()
+
+    result = generate_diff(
+        base_path / 'file1.json',
+        base_path / 'file2.json',
+    )
+
+    assert result == expected
